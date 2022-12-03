d8f487804c05cf6a982c3df6354bead2ab620cb0
Piotr Jaroński <pjaronski@atlassian.com>
2022-12-03 08:20:31 +0100

    Day 3

---
+++ src/main/kotlin/day03/Solution03.kt
@@ -0,0 +1,41 @@
+package day03
+
+import Input
+import Solution
+import SolutionPresenter
+
+fun main() = SolutionPresenter.present(Solution03)
+
+object Solution03 : Solution {
+    private fun error(items: String): Char {
+        val bound = items.length / 2
+        val items1 = items.substring(0, bound).toSet()
+        val items2 = items.substring(bound).toSet()
+        val common = items1 intersect items2
+        return common.single()
+    }
+
+    private fun badge(rucksack1: String, rucksack2: String, rucksack3: String): Char {
+        val items1 = rucksack1.toSet()
+        val items2 = rucksack2.toSet()
+        val items3 = rucksack3.toSet()
+        val common = items1 intersect items2 intersect items3
+        return common.single()
+    }
+
+    private fun priority(item: Char): Int = when (item) {
+        in 'a'..'z' -> item - 'a' + 1
+        in 'A'..'Z' -> item - 'A' + 27
+        else -> throw IllegalArgumentException("Cannot determine the priority of item '$item'")
+    }
+
+    override fun part1(input: Input): String = input.lines()
+        .map { error(it) }
+        .sumOf { priority(it) }
+        .toString()
+
+    override fun part2(input: Input): String = input.lines()
+        .chunked(3) { (r1, r2, r3) -> badge(r1, r2, r3) }
+        .sumOf { priority(it) }
+        .toString()
+}
---
+++ src/test/kotlin/day03/Solution03Test.kt
@@ -0,0 +1,9 @@
+package day03
+
+import SolutionTest
+
+class Solution03Test : SolutionTest() {
+    override val solution = Solution03
+    override val expected1 = "157"
+    override val expected2 = "70"
+}
