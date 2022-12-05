b790bdc9ba08649bd471084b2d834520755dd50a
Piotr Jaroński <pjaronski@atlassian.com>
2022-12-05 08:23:03 +0100

    Refactor

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -2,11 +2,11 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 class Input(clazz: Class<*>, name: String = "input.txt") {
-    private val lines = clazz.getResource(name)!!
+    private val lines: List<String> = clazz.getResource(name)!!
         .toURI()!!
         .let { Paths.get(it) }
         .let { Files.lines(it) }
         .toList()
 
-    fun lines() = lines
+    fun lines(): List<String> = lines
 }
